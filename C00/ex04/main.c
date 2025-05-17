0440b38bb94d18ba4dba8f6cc624afe7dbf07220
Igors Subocs <igorssubocs@gmail.com>
2025-05-17 12:26:48 +0200

    C00

--- C00/ex04/main.c
+++ C00/ex04/main.c
@@ -1,10 +1,18 @@
 void	ft_is_negative(int n);
 
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
 int	main(void)
 {
 	ft_is_negative(-1);
+	ft_putchar('\n');
 	ft_is_negative(0);
+	ft_putchar('\n');
 	ft_is_negative(1);
+	ft_putchar('\n');
 
 	return (0);
 }
